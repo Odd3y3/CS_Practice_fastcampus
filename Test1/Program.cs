ae23b7d924b58bcb9621df0c521badc428d152e3
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-01-16 01:34:28 +0900

    초기 커밋.

---
+++ Test1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int[] array = { 3, 10, 28 };
+int n = 20;
+int result = 100;
+int answerIndex = -1;
+//
+
+for (int i = 0; i < array.Length; i++)
+{
+    int diff = Math.Abs(array[i] - n);
+    if(diff < result)
+    {
+        result = diff;
+        answerIndex = i;
+    }
+}
+
+Console.WriteLine(result);
+Console.WriteLine(answerIndex);
+
+string str = "asdf";
+foreach(var v in str)
+{
+    Console.WriteLine(v);
+}
+char aaaa = 'a';
+aaaa.ToString();
+Console.WriteLine(Convert.ToInt32('2'));
+str.Replace("a", "b");
+Console.WriteLine(str);
