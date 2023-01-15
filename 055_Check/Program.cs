f53c461dc1b7a8189f756e2ec36073426ccf5dd0
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-01-16 01:49:21 +0900

    055_Check / commit Testing..

---
+++ 055_Check/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _055_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int count = 5;
+            int score = 0;
+            Random rnd = new Random();
+
+            for(int i = 1; i <= count; i++)
+            {
+                Console.WriteLine("{0}: 다음 두 수의 합은 몇?(총 {1}문제)",i, count);
+                int firstNum = rnd.Next(1, 100);
+                int secondNum = rnd.Next(1, 100);
+                int answer = firstNum + secondNum;
+                Console.WriteLine("{0} + {1} = ??",firstNum, secondNum);
+                string userAnswer = Console.ReadLine();
+                if(userAnswer == answer.ToString())
+                {
+                    Console.WriteLine("== 정답 ==");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine($"오답(정답은: {answer})");
+                }
+            }
+            Console.WriteLine($"{score}문제 맞춤!");
+        }
+    }
+}
